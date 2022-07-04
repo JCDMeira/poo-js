886fd8e5c6da54fced48c447eda00038846d89b0
Jean Meira <jean.meira10@hotmail.com>
2022-07-04 20:03:50 -0300

    feat: add note about oo and js

--- introducao/OO_notes1.js
+++ introducao/OO_notes1.js
@@ -15,4 +15,12 @@
     - uma classe Carro representa todos os carros do mundo;
     - porém, cada Carro distingue do outro, sendo assim eles são objetos;
     - tendo características (propriedades) e funcionalidades próprias (métodos)
+
+    _ OO e javaScript
+
+    - js não é baseada em classes;
+    - porém da para usar OOP;
+    - ES2015 foi inserida a funcionalidade de classes no js;
+    - js possui prototypes, isso faz com que todos os objetos tenham um pai;
+    - além dos seus objetos built-in como: number, boolean, array, object, error etc
 */
