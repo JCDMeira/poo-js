389a8a19e92f2690d24f3c009ea37a6dfe6c3650
Jean Meira <jean.meira10@hotmail.com>
2022-07-04 19:54:22 -0300

    feat: add intro notes

---
+++ introducao/OO_notes1.js
@@ -0,0 +1,12 @@
+/*
+    _ O que é orientação a objetos?
+
+    - Um paradigma de programação baseado em objetos;
+    - Onde os objetos interagem entre si;
+    - e o objeto é uma instância de uma classe;
+    - auxilia no reaproveitamento de código;
+    - torna o código menos confuso em relação ao procedural;
+    - design patterns.
+
+    # paradigma é uma forma de se fazer algo, no caso programar
+*/
