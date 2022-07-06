800a4a34bbd423d9956f7254eaeeecaf7fcebae2
Jean Meira <jean.meira10@hotmail.com>
2022-07-05 21:15:36 -0300

    feat: add notes about object

---
+++ objetos/notes.js
@@ -0,0 +1,8 @@
+/*
+  _ O que é um objeto?
+
+  - um tipo de dados que possui duas características
+  - propriedades: características do objeto
+  - métodos: ações do objeto
+  @ protagonista da POO
+*/
