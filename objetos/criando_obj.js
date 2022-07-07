d982e0bbb19008c0be5165b2d66bb2f7dd7f71b8
Jean Meira <jean.meira10@hotmail.com>
2022-07-06 22:07:34 -0300

    feat: show propertie with two words

--- objetos/criando_obj.js
+++ objetos/criando_obj.js
@@ -2,6 +2,7 @@ let carro = {
   portas: 4,
   cor: "vermelha",
   opicionais: ["teto solar", "blindagem"],
+  "tem blindagem": true,
 };
 
 console.log(carro);
@@ -12,3 +13,5 @@ console.log(carro.portas);
 console.log(carro["cor"]);
 
 carro.opicionais.map((op) => console.log(op));
+
+console.log(carro["tem blindagem"]);
