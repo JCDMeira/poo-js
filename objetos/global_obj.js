a07b4213274aa59db2d3f38cf6dc8cf9a62462f2
Jean Meira <jean.meira10@hotmail.com>
2022-07-09 20:23:09 -0300

    feat: add obj window

---
+++ objetos/global_obj.js
@@ -0,0 +1,8 @@
+/*
+  _ o objeto global
+
+  - sempre que é iniciada uma pag web traz um obj chamado window
+  - as variáveis globais ficam atreladas a ele como props
+  - os métodos da linguagem também podem ser invocados pela window
+  - o this no escopo global também é referenciado a window
+*/
