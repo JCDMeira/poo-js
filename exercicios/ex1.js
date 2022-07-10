a16ab526073bfc382f1d246114df71509b0e02bc
Jean Meira <jean.meira10@hotmail.com>
2022-07-10 13:55:55 -0300

    feat: add exercise 1

---
+++ exercicios/ex1.js
@@ -0,0 +1,18 @@
+/*
+  _ Exercício 1:
+
+  - crie um obj com 3 props
+  - primeira -> uma string
+  - segunda 2 -> number
+  - terceira -> boolean
+*/
+
+const obj = {
+  prop1: "string",
+  prop2: 42,
+  prop3: true,
+};
+
+console.log(obj.prop1);
+console.log(obj.prop2);
+console.log(obj.prop3);
