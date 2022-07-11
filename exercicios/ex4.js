15be21e25abbbc5b4f9ca884c2a4c49662c2d701
Jean Meira <jean.meira10@hotmail.com>
2022-07-11 20:04:46 -0300

    feat: add ex4

---
+++ exercicios/ex4.js
@@ -0,0 +1,28 @@
+/*
+  _ Exercício 3:
+
+  - crie um obj ninja
+  - use constructor function
+  - deve ter as props nome e shurikens
+  - criar o método atirarShuriken  que subtraí uma shuriken e não atira se não tiver mais delas
+*/
+
+function Ninja(nome, shurikens) {
+  this.nome = nome;
+  this.shurikens = shurikens;
+
+  this.atirarShuriken = function () {
+    if (this.shurikens >= 1) {
+      this.shurikens -= 1;
+      console.log("Lança shuriken");
+    } else {
+      console.log("Acabaram as shurikens");
+    }
+  };
+}
+
+const ninja1 = new Ninja("Ninja1", 2);
+ninja1.atirarShuriken();
+ninja1.atirarShuriken();
+ninja1.atirarShuriken();
+console.log(ninja1.nome);
