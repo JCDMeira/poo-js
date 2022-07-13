00270cf15b77251f3034e65a9e63e508b217ba3a
Jean Meira <jean.meira10@hotmail.com>
2022-07-13 19:09:43 -0300

    feat: add notes about inheritance

---
+++ POO_conceitos/05-heranca.js
@@ -0,0 +1,7 @@
+/*
+  _ Herança (inheritance)
+
+  - quando um obj ou classe deriva de uma outra classe
+  - podendo herdar suas props e métodos
+  - fazendo com que você crie classes com o comportamento semelhante, mas para fins distintos
+*/
