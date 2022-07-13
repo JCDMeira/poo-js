dad3850544acce78c2e722d4118407991f9f05c1
Jean Meira <jean.meira10@hotmail.com>
2022-07-13 19:09:09 -0300

    feat: add notes about encapsulation

---
+++ POO_conceitos/03-encapsulacao.js
@@ -0,0 +1,8 @@
+/*
+  _ Encapsulação (encapsulation)
+
+  - quando um obj contém, ou encapsula, dados ou meios de fazer algo com os dados (usando métodos)
+  - um outro aspecto da encapsulação é ter props e métodos públicos, privados ou protegidos
+  - no js não se tem estes meios de forma nativa, tudo é público
+  - porém pode-se contornar isso
+*/
