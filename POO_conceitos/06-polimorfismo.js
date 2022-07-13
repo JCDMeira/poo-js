b99bb4607db7e52a43ab42e7cc1320bfcde03903
Jean Meira <jean.meira10@hotmail.com>
2022-07-13 19:09:58 -0300

    feat: add notes about polymorphism

---
+++ POO_conceitos/06-polimorfismo.js
@@ -0,0 +1,7 @@
+/*
+  _ Polimorfismo (polymorphism)
+
+  - é a possibilidade de utilizar um métodos de uma classe pai de uma maneira diferente
+  - que se adapte as necessidade de um novo obj, sem precisar alterar o método do obj pai
+  - importante citar que o polimorfismo utiliza o conceito de herança
+*/
