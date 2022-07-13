38a98ac34d3e6d57fb611b451e310be0d6947bd1
Jean Meira <jean.meira10@hotmail.com>
2022-07-13 19:08:55 -0300

    feat: add notes about classes

---
+++ POO_conceitos/02-classes.js
@@ -0,0 +1,10 @@
+/*
+  _ Classes
+
+  - É como o molde do objeto
+  - geralmente se criam diversos objetos da mesma classe
+  - normalmente possui as props e métodos que os objs vão usar
+  - a classe no js foi introduzida na versão ES6 da linguagem
+
+  # no geral um sistema tem menos classes que objs
+*/
