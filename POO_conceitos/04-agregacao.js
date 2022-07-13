e7f559a9ecc801afcaea20b4f508b855010dfb99
Jean Meira <jean.meira10@hotmail.com>
2022-07-13 19:09:30 -0300

    feat: add notes about aggregation

---
+++ POO_conceitos/04-agregacao.js
@@ -0,0 +1,11 @@
+/*
+  _ Agregação (aggregation)
+
+  - também conhecido como composição (composition)
+  - o ato de combinar diversos objetos em um maior
+  - isso serve para não termos um objeto muito grande e complexo
+  - objeto grande = SalaDeAula
+  - objeto com aggregation = SalaDeAula com Aluno, Cadeira, Lousa etc
+  - A sala de aula foi dividida em diversos objetos
+    - cada obj tem sua responsabilidade
+*/
