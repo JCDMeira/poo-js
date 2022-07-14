1d18a1ef655cfbb47a536f46f911350d49e37c22
Jean Meira <jean.meira10@hotmail.com>
2022-07-14 18:51:06 -0300

    feat: add notes about built-in objects

---
+++ built-in-objs/01-conceito.js
@@ -0,0 +1,9 @@
+/*
+  _ built-in objects
+
+  - objs que são criados pelos devs da linguagem
+  - que originam strings, arrays e objs
+  - objs criados heradm suas props e métodos
+  - um aspecto da herança
+  - no js isso é mais conhecido pelo conceito de prototype
+*/
