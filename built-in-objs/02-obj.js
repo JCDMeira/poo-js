c3b7d02a442c259a31516e1176097aae9fdb665c
Jean Meira <jean.meira10@hotmail.com>
2022-07-14 18:54:02 -0300

    feat: add notes about Object

---
+++ built-in-objs/02-obj.js
@@ -0,0 +1,12 @@
+/*
+  _ Object
+
+  - pai de todos objs em js
+  - possui props e métodos, mesmo o obj estando vazio
+  - pode ser criado via new
+*/
+
+let o = new Object();
+
+console.log(o.toString()); //? retorna a representação de um obj em string
+console.log(o.valueOf()); //? retorna o próprio obj
