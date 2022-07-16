76a6a5338f5cef17d4be13a9ab12226346bf91f9
Jean Meira <jean.meira10@hotmail.com>
2022-07-16 11:04:25 -0300

    feat: add notes about Boolean

---
+++ built-in-objs/05-boolean.js
@@ -0,0 +1,13 @@
+/*
+  _ Boolean
+
+  - boolean também é um obj para valores booleano (true e false)
+  - é possível cruar com o new e o método valueof() da valor do booleano
+  - é um obj que não tem métodos
+
+  # por ser tipo primário ele não é uma instancia de Boolean
+*/
+
+let x = new Boolean(false);
+
+console.log(x.valueOf());
