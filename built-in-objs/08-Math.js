2069037d802a730876a2400676b76609694fcc2f
Jean Meira <jean.meira10@hotmail.com>
2022-07-17 17:37:41 -0300

    feat: add notes about Math

---
+++ built-in-objs/08-Math.js
@@ -0,0 +1,24 @@
+/*
+  _ Math
+
+  - Um obj com props e métodos matemáticos
+  - da pra usar constantes como pi ou gerar números aleatórios
+  - há métodos de arredondamento como:
+    - floor -> arredonda pra baixo
+    - ceil -> retorna o menor número inteiro maior ou igual ao argumento, arredonda pra cima
+    - round ->  retorna o valor de um número arredondado para o inteiro mais proximo.
+  - pode calcular potência e raiz
+*/
+
+console.log(Math.PI);
+console.log(Math.LN2); // log natural (base e)
+console.log(Math.LN10);
+console.log(Math.random());
+
+console.log(Math.floor(123.22));
+console.log(Math.ceil(123.22));
+console.log(Math.round(123.2));
+console.log(Math.round(123.5));
+console.log(Math.round(123.8));
+console.log(Math.pow(12, 2));
+console.log(Math.sqrt(9));
