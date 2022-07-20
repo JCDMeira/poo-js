607d6a5114ef8c34a2b331c0ead6a3163724f0a3
Jean Meira <jean.meira10@hotmail.com>
2022-07-20 20:05:28 -0300

    feat: add btnPress

--- calculadora/js/scripts.js
+++ calculadora/js/scripts.js
@@ -5,8 +5,33 @@ class Calculator {
     this.reset = 0;
   }
 
+  checkLastDigit(input, upperValue, reg) {
+    if (
+      !reg.test(input) &&
+      !reg.test(upperValue.substr(upperValue.length - 1))
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   btnPress() {
-    console.log("apertou");
+    let input = this.textContent;
+    let upperValue = calc.upperValue.textContent;
+
+    //verifica se só tem números
+    let reg = new RegExp("^\\d+$");
+
+    if (calc.checkLastDigit(input, upperValue, reg)) {
+      return false;
+    }
+
+    if (upperValue === "0") {
+      calc.upperValue.textContent = input;
+    } else {
+      calc.upperValue.textContent += input;
+    }
   }
 }
 
