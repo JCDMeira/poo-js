fe5e130f9ea1300feef5a852215948db59d99e47
Jean Meira <jean.meira10@hotmail.com>
2022-07-24 15:02:48 -0300

    feat: add notes about inheritance

---
+++ heranca/01-heranca.js
@@ -0,0 +1,8 @@
+/*
+  _ a herança e o js
+
+  - reutilização de código
+  - as props e métpdps são passadas para outros objs filhos
+  - a herança do js pode ser aplicada via pototype chain
+  - porém há outras maneiras de atingir este objetivo
+*/
