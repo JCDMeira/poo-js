50c363cd50bb08ee2bd1b90585caee048819232e
Jean Meira <jean.meira10@hotmail.com>
2022-07-27 17:28:04 -0300

    feat:add note about constructor in classes

---
+++ classes/03-constructor.js
@@ -0,0 +1,7 @@
+/*
+  _ Curiosidades sobre o constructor
+
+  - é utilizado apenas para inicializar valores de props
+  - você só pode utilizar um constructor por classe
+  - o constructor pode chamar a classe pai por uma instrução super (o que ajuda na herança)
+*/
