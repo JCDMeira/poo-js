09f96f4418363cd989910569ed3c1a6486d5daad
Jean Meira <jean.meira10@hotmail.com>
2022-07-28 19:45:56 -0300

    feat: add notes about typescript

---
+++ typescript/typescript.js
@@ -0,0 +1,23 @@
+/*
+  _ o que é typeScript?
+
+  - é um superset para uma linguagem js
+  - que alguns consideram uma linguagem
+  - traz algumas funcionalidades novas como por exemplo
+    - types
+    - features de versões fututas do js
+    - interfaces
+    - generics
+    - decorators
+  - nós não executamos ts, compilamos em js e utilizamos a versão gerada
+  - utiliza-se em conjunto de outras tecnologias, por ex, react
+*/
+
+/*
+  _ intalando
+
+  - instalar node
+  - rodar:
+    ? npm install -g typescript
+
+*/
